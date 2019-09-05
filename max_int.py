297282bf4b322b6873c0f405b9614a81ab60a7fe
Ingunn Káradóttir <ingunn19@ru.is>
2019-09-05 10:25:18 +0000

    Algorithm done, starting programming

---
+++ max_int.py
@@ -0,0 +1,6 @@
+# Program finds the maximum positive integer input by user
+
+# get continuous input from the user until a negative value is entered
+# as the user inputs values save it as a variable
+# if the next value input is larger than the one currently saved as the max overwrite with the higher value
+# when a negative value is input stop asking for input print the currently saved highest input and end the program
