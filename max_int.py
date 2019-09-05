3fc372e6324891f2c1e7aa05bdff64c3b3ba30f6
Ingunn Káradóttir <ingunn19@ru.is>
2019-09-05 10:26:23 +0000

    Testing if code works

--- max_int.py
+++ max_int.py
@@ -1,6 +1,12 @@
 # Program finds the maximum positive integer input by user
 
+max_int = 0
 # get continuous input from the user until a negative value is entered
 # as the user inputs values save it as a variable
-# if the next value input is larger than the one currently saved as the max overwrite with the higher value
-# when a negative value is input stop asking for input print the currently saved highest input and end the program
+num_int = int(input("Input a number: "))    # Do not change this line
+while num_int >= 0:          # when a negative value is input stop asking for input
+    if num_int > max_int:    # if the next value input is larger than the one currently saved as the max overwrite with the higher value
+        max_int = num_int    
+
+# print the currently saved highest input and end the program
+print("The maximum is", max_int)    # Do not change this line
