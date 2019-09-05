e0d8c65426a08ca6556259c8117e804ecd508cee
Ingunn Káradóttir <ingunn19@ru.is>
2019-09-05 10:29:21 +0000

    Forgot to add second input line, code fixed and complete

--- max_int.py
+++ max_int.py
@@ -7,6 +7,7 @@ num_int = int(input("Input a number: "))    # Do not change this line
 while num_int >= 0:          # when a negative value is input stop asking for input
     if num_int > max_int:    # if the next value input is larger than the one currently saved as the max overwrite with the higher value
         max_int = num_int    
+    num_int = int(input("Input a number: "))
 
 # print the currently saved highest input and end the program
 print("The maximum is", max_int)    # Do not change this line
